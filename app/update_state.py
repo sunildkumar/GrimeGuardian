2255a0fae0b54582ef5997b72a728e16c112e28b
senorkumar <sdkumar@hmc.edu>
2023-12-12 17:35:18 -0800

    moving to pi

--- app/update_state.py
+++ app/update_state.py
@@ -26,14 +26,14 @@ def update_sink_state(
 
                     # we only care about answers that are more recent than the last update to the state
                     if timestamp > state.get_state().sink_state_timestamp:
-                        # if the answer is YES, then the sink is dirty and we only need to update the sink_state and sink_timestamp
+                        # if the answer is YES, then the sink is dirty
                         if answer.result.label == "YES":
                             state.update_state(
                                 sink_state=answer.result.label,
                                 sink_state_timestamp=timestamp,
                                 sink_state_iq_id=answer.id,
                             )
-                        # if the answer is NO, then the sink is clean and we need to update the sink_state, sink_timestamp, and last_sink_clean_timestamp
+                        # if the answer is NO, then the sink is clean
                         elif answer.result.label == "NO":
                             state.update_state(
                                 sink_state=answer.result.label,
